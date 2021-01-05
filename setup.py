067087950bff253fae9eb7409ad79b7e85558662
Ranjith Bojraj <abranjith@gmail.com>
2021-01-05 00:42:43 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+
+from setuptools import setup
+
+# Package meta-data.
+NAME = 'unblock'
+DESCRIPTION = 'Simple solution to convert synchronous calls to asynchronous'
+URL = 'https://github.com/abranjith/unblock'
+EMAIL = 'abranjith@gmail.com'
+AUTHOR = 'ranjith'
+VERSION = '0.0.1'
+README_CONTENT_TYPE = 'text/markdown'
+
+# What packages are required for this module to be executed?
+REQUIRED = []
+
+#here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type=README_CONTENT_TYPE,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    python_requires='>=3.7.0',
+    # If your package is a single module, use this instead of 'packages':
+    packages=['unblock', 'unblock.io'],
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='Apache',
+    classifiers=[
+        'License :: OSI Approved :: Apache License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+)
